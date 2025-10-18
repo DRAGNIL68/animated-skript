8c5c01e9f2d5c7a4818873efc3d86b674662a05b
DRAGNIL68 <DRAGNIL68@users.noreply.github.com>
2025-10-18 21:19:33 +0100

    - updated README.md

--- src/main/java/net/outmoded/animated_skript/skript/conditions/CondLoadedModelExists.java
+++ src/main/java/net/outmoded/animated_skript/skript/conditions/CondLoadedModelExists.java
@@ -20,7 +20,7 @@ import static org.bukkit.Bukkit.getServer;
 public class CondLoadedModelExists extends Condition {
 
     static {
-        Skript.registerCondition(CondLoadedModelExists.class, "[animated-skript] (:loaded-model|active-model) %string% exits");
+        Skript.registerCondition(CondLoadedModelExists.class, "[animated-skript] loaded-model %string% exits");
     }
 
     private boolean type; // if true = loaded-models | if false = active-models
@@ -29,7 +29,6 @@ public class CondLoadedModelExists extends Condition {
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
-        type = parseResult.hasTag("loaded-models");
         text = (Expression<String>) expressions[0];
 
         return true;
@@ -42,23 +41,8 @@ public class CondLoadedModelExists extends Condition {
 
     @Override
     public boolean check(Event event) {
-        if (type){
+        return ModelManager.getInstance().loadedModelExists(text.toString());
 
-            return ModelManager.getInstance().loadedModelExists(text.toString());
-        }
-        String uuidAsString = text.getSingle(event);
-        UUID uuid = null;
-        try {
-            uuid = UUID.fromString(uuidAsString);
-
-        }catch (IllegalArgumentException e){
-            if (Config.debugMode())
-                getServer().getConsoleSender().sendMessage(ChatColor.RED + "Error " + text.getSingle(event));
-
-            return false;
-
-        }
-        return ModelManager.getInstance().activeModelExists(uuid);
     }
 
 
